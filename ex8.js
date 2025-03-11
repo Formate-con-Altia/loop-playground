824d89085fd50746639cc21139db0f959d5cf8d5
omiras <oscar.miras@gmail.com>
2025-03-11 21:16:23 +0100

    done ex8

--- ex8.js
+++ ex8.js
@@ -1,8 +1,8 @@
 /**
  * Bucles while
  *
- * Modifica el bucle while para que escriba por el terminal 
- * escriba 5 veces exactamente "Ejecuto!". Actualmente lo escribe 6 veces. Porque?
+ * Modifica el bucle while para que escriba por el terminal  5 veces exactamente la palabra "Ejecuto!".
+ * Actualmente lo escribe 6 veces. ¿Porque?
  * 
  * Reestricción: NO puedes cambiar el valor de las variables 'veces' ni 'i' !!
  */
@@ -10,7 +10,11 @@
 let veces = 5; // NO ME CAMBIES
 let i = 0; /// NO ME CAMBIES
 
-while (i<=veces) {
+while (i<veces) {
     console.log("Ejecuto!");
     i++;
 }
+
+/**
+ * Solución: En la condición de finalización ponía i<=veces. Esto quiere decir que el bucle se ejecutaba para los valores de i: 0, 1, 2, 3, 4, y 5. Es decir, se ejecutaba 6 veces. Cambiamos el operador a i < veces para que , cuando la i valga 5, como 5 < 5 -> false , esto hará que salgamos del bucle.
+ */
