f16fa5f3aa34c2b355cc3a00d46f3b19e2ba28dc
omiras <oscar.miras@gmail.com>
2025-03-11 20:39:19 +0100

    ex6 done

--- ex6.js
+++ ex6.js
@@ -14,9 +14,20 @@ let numeroFinal = 5;
 for (var i = -5; i < numeroFinal; i++) {
   console.log("Voy a ejecutar el condicional para el número: " + i);
 
-  console.log("Soy un número positivo!");
-  console.log("Soy un núemro negativo!");
+  if (i > 0) {
+    console.log("Soy un número positivo!");
+  } else if (i < 0) {
+    console.log("Soy un núemero negativo!");
+  } else {
+    console.log("Soy el 0");
+  }
 }
+
+/**
+ * Una forma de mirar si un número es positivo es mirar si su valor es mayor que 0.
+ * Si no lo es, entonces comprobamos si es un número negativo.
+ * A pesar que no lo indique el enunciado...si un número no es ni positivo ni negativo...¡entonces podems decir que es el número 0!
+ */
 
 /**
  * Resultado esperado: https://oscarm.tinytake.com/msc/ODc4MzYyNF8yMjIwMzg1OA
