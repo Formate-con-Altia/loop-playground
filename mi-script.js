bd6679cfbf2788754adb4e0c6537663d4d68497f
omiras <oscar.miras@gmail.com>
2025-03-11 20:29:31 +0100

    thanks Sergio

--- mi-script.js
+++ mi-script.js
@@ -19,7 +19,7 @@ dibujarCirculo();
 dibujarCirculo();
 
 /**
- * BONUS: Modifica el bucle para introducir la variable 'tocaHexagono'. El bucle debe funcionar como antes, pero ahora, cuando nos encotremos en la iteración número 'tocaHexagono', se debe pintar un hexagono amarillo. Investiga el fichero "mi-script.js" para averiguar como podríamos pintar un hexágono
+ * BONUS: Modifica el bucle para introducir la variable 'tocaHexagono'. El bucle debe funcionar como antes, pero ahora, cuando nos encotremos en la iteración número 'tocaHexagono', se debe pintar un hexagono amarillo. Investiga el fichero "script-externo.js" para averiguar como podríamos pintar un hexágono
  */
 let tocaHexagono = 4;
 
