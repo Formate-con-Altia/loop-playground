d391665d4d5c3db2f0f013d69178c74ae9fa7861
omiras <oscar.miras@gmail.com>
2025-03-11 20:20:59 +0100

    ex1 solved

--- ex1.js
+++ ex1.js
@@ -2,7 +2,11 @@
  * Ejercicio 1: Escribe un bucle que muestre por consola 10 veces la frase "Estoy en un bucle!" El ejercicio está CASI bien, pero falta arreglar alguna cosa. Ejecútalo para ver que sucede.
  */
 
-for (var i = 0; i < 5; i++) {
+/** 
+ * Solución: El bucle se ejecuta solamente 5 veces. Es necesario modificar la condición de finalización del bucle para que se ejecute 10 veces
+*/
+
+for (var i = 0; i < 10; i++) {
   console.log("Estoy en un bucle!");
 }
 
